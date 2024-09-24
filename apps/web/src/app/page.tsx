cc6d53f8f788a5baa2c9792bfcef22e0fab72f86
rjtprasad <prasadrajat28@gmail.com>
2024-09-24 16:00:31 +0530

    code validation

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,5 @@
-import {add } from "@autospace/sample-lib"
+import { add } from '@autospace/sample-lib'
 
 export default function Home() {
-  return <main>Hello {add(10, 10)}</main>;
+  return <main>Hello {add(10, 10)}</main>
 }
