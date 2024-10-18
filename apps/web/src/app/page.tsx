0789a043e551f3c952d1611f7d194ee1312b2d5a
rjtprasad <prasadrajat28@gmail.com>
2024-10-18 19:20:44 +0530

    added graphql codegen for generating types for GraphQL query, mutation, and fragment.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,24 @@
+'use client'
 import { add } from '@autospace/sample-lib'
+import { useQuery } from '@apollo/client'
+import { CompaniesDocument } from '@autospace/network/src/gql/generated'
 
 export default function Home() {
-  return <main>Hello {add(10, 10)}</main>
+  const { data } = useQuery(CompaniesDocument, {
+    variables: {},
+  })
+
+  return (
+    <main>
+      Hello {add(10, 10)}
+      <div>
+        {data?.companies.map((company) => (
+          <div className="p-4 bg-gray-100 rounded" key={company.id}>
+            <div>{company.displayName}</div>
+            <div>{company.description}</div>
+          </div>
+        ))}
+      </div>
+    </main>
+  )
 }
