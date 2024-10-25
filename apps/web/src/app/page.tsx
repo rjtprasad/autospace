ad9bcb6ff056f33c05a16cf8819f1a90eef28dc9
rjtprasad <prasadrajat28@gmail.com>
2024-10-25 19:13:28 +0530

    added sidebar and navbar

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,38 +1,22 @@
 'use client'
-import { add } from '@autospace/sample-lib'
 import { useQuery } from '@apollo/client'
 import { CompaniesDocument } from '@autospace/network/src/gql/generated'
-import { Button } from '@autospace/ui/src/components/atoms/Button'
-import { useSession, signOut } from 'next-auth/react'
-import Link from 'next/link'
 
 export default function Home() {
   const { data } = useQuery(CompaniesDocument, {
     variables: {},
   })
 
-  const { data: sessionData } = useSession()
-
   return (
-    <main>
-      <main>
-        <div>
-          {sessionData?.user?.uid ? (
-            <Button onClick={() => signOut()}>Signout</Button>
-          ) : (
-            <Link href="/login">Login</Link>
-          )}
-        </div>
-        Hello {add(10, 100)}
-        <div>
-          {data?.companies.map((company) => (
-            <div className="p-4 rounded" key={company.id}>
-              <div>{company.displayName}</div>
-              <div>{company.description}</div>
-            </div>
-          ))}
-        </div>
-      </main>
+    <main className="p-8">
+      <div>
+        {data?.companies.map((company) => (
+          <div className="p-4 rounded" key={company.id}>
+            <div>{company.displayName}</div>
+            <div>{company.description}</div>
+          </div>
+        ))}
+      </div>
     </main>
   )
 }
