5cdc0ec9de5739f356d8dbc39edde03b1cc1bb0d
rjtprasad <prasadrajat28@gmail.com>
2024-11-13 22:44:10 +0530

    Search Page: Added loader and no parking slot alert

--- libs/ui/src/components/organisms/search/ShowGarages.tsx
+++ libs/ui/src/components/organisms/search/ShowGarages.tsx
@@ -3,6 +3,9 @@ import { SearchGaragesDocument } from '@autospace/network/src/gql/generated'
 import { useEffect } from 'react'
 import { GarageMarker } from './GarageMarker'
 import { useConvertSearchFormToVariables } from '@autospace/forms/src/adapters/searchFormAdapter'
+import { Panel } from '../map/Panel'
+import { Loader } from '../../molecules/Loader'
+import { IconInfoCircle } from '@tabler/icons-react'
 
 export const ShowGarages = () => {
   const [searchGarages, { data, loading, error }] = useLazyQuery(
@@ -17,8 +20,27 @@ export const ShowGarages = () => {
     }
   }, [searchGarages, variables])
 
+  if (data?.searchGarages.length === 0) {
+    return (
+      <Panel
+        position="center-center"
+        className="bg-white/50 shadow border-white border backdrop-blur-sm"
+      >
+        <div className="flex items-center justify-center gap-2">
+          <IconInfoCircle /> <div>No parking slots found in this area.</div>
+        </div>
+      </Panel>
+    )
+  }
+
   return (
     <>
+      {loading ? (
+        <Panel position="center-bottom">
+          <Loader />
+        </Panel>
+      ) : null}
+
       {data?.searchGarages.map((garage) => (
         <GarageMarker key={garage.id} marker={garage} />
       ))}
