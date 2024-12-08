3233dbe1083f0e8107e8a65494e097c977e7bc10
rjtprasad <prasadrajat28@gmail.com>
2024-12-08 20:45:48 +0530

    Fix map direction mode from walking to driving

--- libs/ui/src/components/organisms/Sidebar.tsx
+++ libs/ui/src/components/organisms/Sidebar.tsx
@@ -1,4 +1,9 @@
-import { Dialog, DialogPanel, Transition, TransitionChild } from '@headlessui/react'
+import {
+  Dialog,
+  DialogPanel,
+  Transition,
+  TransitionChild,
+} from '@headlessui/react'
 import { Fragment, ReactNode } from 'react'
 import { IconX } from '@tabler/icons-react'
 
@@ -9,7 +14,12 @@ export interface ISidebarProps {
   blur?: boolean
 }
 
-export const Sidebar = ({ open, setOpen, children, blur=true }: ISidebarProps) => {
+export const Sidebar = ({
+  open,
+  setOpen,
+  children,
+  blur = true,
+}: ISidebarProps) => {
   return (
     <Transition show={open} as={Fragment}>
       <Dialog
@@ -17,7 +27,8 @@ export const Sidebar = ({ open, setOpen, children, blur=true }: ISidebarProps) =
         className="fixed inset-0 z-50 overflow-hidden"
         onClose={() => setOpen(false)}
       >
-          {blur ? <TransitionChild
+        {blur ? (
+          <TransitionChild
             as={Fragment}
             enter="transition-opacity ease-linear duration-150"
             enterFrom="opacity-0"
@@ -27,30 +38,31 @@ export const Sidebar = ({ open, setOpen, children, blur=true }: ISidebarProps) =
             leaveTo="opacity-0"
           >
             <div className="absolute inset-0 bg-black/30 backdrop-blur-sm" />
-          </TransitionChild> : null}
+          </TransitionChild>
+        ) : null}
 
-          <div className="fixed inset-y-0 right-0 flex max-w-full bg-white  ">
-            <TransitionChild
-              as={Fragment}
-              enter="transform transition ease-in-out duration-150"
-              enterFrom="translate-x-full"
-              enterTo="translate-x-0"
-              leave="transform transition ease-in-out duration-100"
-              leaveFrom="translate-x-0"
-              leaveTo="translate-x-full"
-            >
-              <DialogPanel  className="flex flex-col w-screen max-w-md p-3">
-                <button
-                  type="button"
-                  className="absolute top-0 right-0 z-10 m-2 ml-auto rounded-full"
-                  onClick={() => setOpen(false)}
-                >
-                  <IconX className="w-6 h-6 p-1" aria-hidden="true" />
-                </button>
-                {children}
-              </DialogPanel >
-            </TransitionChild>
-          </div>
+        <div className="fixed inset-y-0 right-0 flex max-w-full bg-white  ">
+          <TransitionChild
+            as={Fragment}
+            enter="transform transition ease-in-out duration-150"
+            enterFrom="translate-x-full"
+            enterTo="translate-x-0"
+            leave="transform transition ease-in-out duration-100"
+            leaveFrom="translate-x-0"
+            leaveTo="translate-x-full"
+          >
+            <DialogPanel className="flex flex-col w-screen max-w-md p-3">
+              <button
+                type="button"
+                className="absolute top-0 right-0 z-10 m-2 ml-auto rounded-full"
+                onClick={() => setOpen(false)}
+              >
+                <IconX className="w-6 h-6 p-1" aria-hidden="true" />
+              </button>
+              {children}
+            </DialogPanel>
+          </TransitionChild>
+        </div>
       </Dialog>
     </Transition>
   )
